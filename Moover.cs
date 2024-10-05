d6ee927413a320c2d718f180f2f139fd60fa9d6d
Makar0001 <mak624336@gmail.com>
2024-10-05 16:00:37 +0300

    Update Moover.cs

--- Moover.cs
+++ Moover.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Moover : MonoBehaviour
+{
+void Moove()
+{
+ transform.position += Vector3.Down * speed * Time.deltaTime;
+}
+public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
